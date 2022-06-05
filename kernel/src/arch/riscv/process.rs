d8729bbe88b58940fc60251a6186ad50a0a761ca
bunnie <bunnie@kosagi.com>
2022-06-05 17:23:31 +0800

    extend kernel stack area to accommodate ditherpunk -- but needs build system rework

--- kernel/src/arch/riscv/process.rs
+++ kernel/src/arch/riscv/process.rs
@@ -13,6 +13,9 @@ use crate::services::ProcessInner;
 use xous_kernel::{ProcessInit, ProcessStartup, ThreadInit, PID, TID};
 
 // use crate::args::KernelArguments;
+#[cfg(feature="ditherpunk")]
+pub const DEFAULT_STACK_SIZE: usize = 1024 * 1024; //131072;
+#[cfg(not(feature="ditherpunk"))]
 pub const DEFAULT_STACK_SIZE: usize = 131072;
 pub const MAX_PROCESS_COUNT: usize = 64;
 // pub use crate::arch::mem::DEFAULT_STACK_TOP;
